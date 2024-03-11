f973d33f75cb649735974e7fb6c74a2df7c1688c
bishamkumar12 <158035225+bishamkumar12@users.noreply.github.com>
2024-03-11 17:05:52 +0500

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "bisham"
+let age = 18
+let isLoggedIn = false
+
+// number
+// bigint
+// string 
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
