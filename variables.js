8894e8a07aece3806ffffd93593689e27fbc5c4a
bishamkumar12 <158035225+bishamkumar12@users.noreply.github.com>
2024-03-11 16:34:52 +0500

    play with var and let

---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId = 12341
+let accountEmail = "bisham@google.com"
+var accountPasswerd = "1234"
+accountCity = "Mithi"
+let accountState
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPasswerd, accountCity, accountState])
+
+// there are two ways of print. 1. console.log() 2. console.table([])
+
+// two ways to declare variable. by let and by var 
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
