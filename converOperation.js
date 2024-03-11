664130f6c38f8c90c1b68aeee8ff3289d77f9fe6
bishamkumar12 <158035225+bishamkumar12@users.noreply.github.com>
2024-03-11 17:47:31 +0500

    conversion of datatypes

---
+++ converOperation.js
@@ -0,0 +1,17 @@
+let score = "33" 
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+let isLoggedIn = 'bisham'
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// "" => false
+// "bisham" => true
+
